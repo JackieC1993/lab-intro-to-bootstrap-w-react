3e37a1baa67b249fee61f3b57ce2de843e807078
JackieC1993 <jackiecheung@pursuit.org>
2023-08-18 01:52:07 -0400

    added postbylocations, travelcards

--- src/components/PostByLocation.jsx
+++ src/components/PostByLocation.jsx
@@ -1,29 +1,42 @@
 import React from "react";
 
 function PostByLocation() {
+  function generateLocationPost() {
+    //starts as an empty object but it will be populated as it iterates though
+    let countNum = [];
+    postData.forEach((couObj) => {
+      if (countNum[couObj.location]) {
+        countNum[couObj.location]++;
+      } else {
+        countNum[couObj.location] = 1;
+      }
+    });
+    const elements = [];
+    for (let key in countNum) {
+      elements.push(
+        <>
+          <div className="border-bottom pb-2">{key}</div>
+          <div className="border-bottom pb-2 w-auto">
+            <span className="badge text-bg-secondary">{countNum[key]}</span>
+          </div>
+        </>
+      );
+    }
+
+    return <>{elements}</>;
+  }
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          Logo
-        </a>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link 1
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link 2
-              </a>
-            </li>
-          </ul>
+      <div className="row-align-items-start">
+        <div className="col">
+          Posts by <span className="text-warning">location</span>
         </div>
       </div>
-    </nav>
+      <div className="row align-items-start row-cols-1 row-cols-md-2 d-inline-flex flex-wrap">
+        <div className="w-50 border-bottom pb-2">Location</div>
+        <div className="border-bottom pb-2 w-auto">Posts</div>
+        {generateLocationPost()}
+      </div>
     </>
   );
 }
--- src/components/TravelCards.jsx
+++ src/components/TravelCards.jsx
@@ -1,32 +1,36 @@
 import React from "react";
+import images from "../images/index";
 
-function Table({ postsData }) {
-  const locationCounts = {};
-
-  postsData.forEach((post) => {
-    locationCounts[post.location] = (locationCounts[post.location] || 0) + 1;
-  });
-
+function TravelCards({ post }) {
   return (
-    <div className="container mt-4">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Location</th>
-            <th>Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(locationCounts).map((location) => (
-            <tr key={location}>
-              <td>{location}</td>
-              <td>{locationCounts[location]}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      {post.map((item) => {
+        let pic = item.location.toLowercCase();
+        if (pic === "San Francisco") pic = "San Francisco";
+        else if (pic === "San Diego") pic = "San-Diego";
+        return (
+          <div key={item.id} className="col-md">
+            <div className="card">
+              <img
+                className="post-image-height"
+                src={images[pic]}
+                alt="img-location"
+              />
+              <div className="card-body">
+                <p>{item.title}</p>
+                <h6 className="text-muted">{item.location}</h6>
+                <p>{(item.content, substring(0, 55))}...</p>
+
+                <div className="d-flex justify-content-end">
+                  <button type="click" className="btn btn-warning ml-auto">
+                    Go to Post
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
-
-export default TravelCards;
